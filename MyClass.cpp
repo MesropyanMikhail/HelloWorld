514b90791fd0f681d3f4a3b95de8617f17077982
MesropyanMikhail <45973298+MesropyanMikhail@users.noreply.github.com>
2018-12-20 16:49:13 +0200

    Add files via upload

---
+++ MyClass.cpp
@@ -0,0 +1,15 @@
+#include "MyClass.h"
+#include <iostream>
+using namespace std;
+
+MyClass::MyClass()
+{
+    cout << "Constructor" << endl;
+    //ctor
+}
+
+MyClass::~MyClass()
+{
+    cout << "Destructor" << endl;
+    //ctor
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MyClass.h"
+
+using namespace std;
+
+int main()
+{
+    MyClass obj;
+
+    return 0;
+}
